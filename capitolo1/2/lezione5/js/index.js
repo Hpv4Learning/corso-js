ccbcf852846ac3746d4afba516fa472ddb3b5dbe
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-09-11 20:35:41 +0200

    helpers for dev functions

---
+++ capitolo1/2/lezione5/js/index.js
@@ -0,0 +1,12 @@
+// document.write("Ciao");
+// alert("ciao");
+// console.log("ciao");
+
+const esempio = {
+  text: "Testo",
+  numero: 2,
+};
+
+document.write(esempio);
+alert(esempio);
+console.log(esempio);
