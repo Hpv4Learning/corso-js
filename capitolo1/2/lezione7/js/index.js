b66c521d5fb3d9e76c9b0ee700ef67cd04b7a177
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-09-12 20:18:39 +0200

    Usare una variabile

---
+++ capitolo1/2/lezione7/js/index.js
@@ -0,0 +1,12 @@
+// Ripetitività difficile da mantenere
+console.log("Ciao");
+console.log("Ciao");
+console.log("Ciao");
+console.log("Ciao");
+
+// let saluto = "Ciao";
+let saluto = "Buongiorno";
+console.log(saluto);
+console.log(saluto);
+console.log(saluto);
+console.log(saluto);
