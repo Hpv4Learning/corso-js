e1e517033d1264a774a94a13118b7e9bdc8e212d
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-09-12 21:01:30 +0200

    Variable naming rules

---
+++ capitolo1/2/lezione8/js/index.js
@@ -0,0 +1,19 @@
+// Regole di assegnazione
+/**
+ * 1 stringhe, numeri, _ e $ sono concessi
+ * 2 Non può iniziare con un numero
+ * 3 Parole riservate
+ * 4 camelCase e nomi descrittivi
+ */
+
+let nome = "Enzo";
+let $cognome = "Goduria";
+// let 12eta = 12
+// let var = 12
+let nomeCompleto = "Enzo Goduria";
+// alternativa
+let nome_completo = "Enzo Goduria";
+//Nomi devono essere descrittivi
+let healthyCategoryFoodName = "Vegetarian";
+// Non Cose del genere
+let x = "";
