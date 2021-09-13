36d40ee952b47a3a76defea0c793146b1c7db4e8
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-13 17:55:58 +0200

    funzionamento operatori assegnazione ed incremento

---
+++ capitolo1/3/lezione13/js/index.js
@@ -0,0 +1,18 @@
+// Ancora numeri
+// +=, -=, *=, /=, ++, --, %
+// Modulo %, operatore che ritorna il resto di una divisione
+
+let numero = 20;
+numero += 10;
+// numero += 10;
+// numero -= 10;
+// numero += 10;
+// numero *= 10;
+// numero++;
+// numero++;
+// numero++;
+// numero++;
+// numero--;
+// numero--;
+
+console.log(numero);
