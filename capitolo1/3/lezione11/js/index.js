864527b281aeda9b04f75db58a1d1ab015c1e8cd
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-13 16:52:13 +0200

    primo esempio - nome completo

---
+++ capitolo1/3/lezione11/js/index.js
@@ -0,0 +1,10 @@
+// Concatenazione di stringhe
+//
+
+const nome = "Enzo";
+const cognome = "Goduria";
+let nomeCompleto;
+nomeCompleto = nome + " " + cognome + ".";
+
+console.log("Ciao, il tuo nome completo è: " + nome + " " + cognome + ".");
+console.log("Ciao, il tuo nome completo è: " + nomeCompleto);
