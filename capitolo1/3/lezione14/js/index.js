41682ab14c276e7008e2abb0223968b6d158476d
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-14 12:45:42 +0200

    Dimostrazione conversione implicita tramite operazioni tra stringhe e operazioni tra numeri e stringhe

---
+++ capitolo1/3/lezione14/js/index.js
@@ -0,0 +1,20 @@
+// Conversione implicita
+
+const nome = "Enzo";
+const cognome = "Goduria";
+const nomeCompleto = nome + " " + cognome;
+console.log(nomeCompleto);
+
+const numero = 15;
+const numero2 = 7;
+const risultato = numero + numero2;
+console.log(risultato);
+
+// operazioni con stringhe
+const prova = nome - cognome;
+console.log(prova); //Nan
+
+const numero3 = "47";
+const numero4 = 8;
+const risultato2 = numero3 - numero4; // usando + concateniamo
+console.log(risultato2);
