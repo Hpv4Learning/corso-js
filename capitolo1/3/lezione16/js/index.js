66f11e2c9c2a32a035033ab0fd02e21e84dfc1d9
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-14 15:23:15 +0200

    Dimostrazione di come accedere e modificare un valore nell array

--- capitolo1/3/lezione16/js/index.js
+++ capitolo1/3/lezione16/js/index.js
@@ -1,13 +1,22 @@
 // Array - cosa sono?
 // [] - 0 index based
 
-let array = ["valore1", "valore2", true, 55, null];
+// let array = ["valore1", "valore2", true, 55, null];
 
-const cane1 = "Bobby";
-const cane2 = "Jack";
-const cane3 = "Joy";
-const cane4 = "Maggie";
+// const cane1 = "Bobby";
+// const cane2 = "Jack";
+// const cane3 = "Joy";
+// const cane4 = "Maggie";
 
-let cani = [cane1, cane2, cane3, cane4];
+// let cani = [cane1, cane2, cane3, cane4];
 
-console.log(cani);
+// console.log(cani);
+
+let amici = ["Mario", "Luigi", "Giacomo", "Jacopo", "Sara"];
+console.log(amici);
+
+let miglioreAmico = amici[2]; // accedo al valore con indice 2 dell'array amici
+console.log(miglioreAmico);
+
+amici[3] = "Andrea";
+console.log(amici[3]);
