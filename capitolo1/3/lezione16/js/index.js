043891e994eb0359d1822c8e2f2bf49c2a62363c
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-14 15:13:02 +0200

    Creazione array, dimostrazione di cosa può contenere

---
+++ capitolo1/3/lezione16/js/index.js
@@ -0,0 +1,13 @@
+// Array - cosa sono?
+// [] - 0 index based
+
+const array = ["valore1", "valore2", true, 55, null];
+
+const cane1 = "Bobby";
+const cane2 = "Jack";
+const cane3 = "Joy";
+const cane4 = "Maggie";
+
+const cani = [cane1, cane2, cane3, cane4];
+
+console.log(cani);
