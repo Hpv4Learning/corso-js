1e3f6206dd9ecd55da4e49647e82e90d73e0d2e7
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-14 12:53:03 +0200

    Ricezione dato dal form, utilizzo di parseInt per gestire le operazioni algebriche

--- capitolo1/3/lezione14/js/index.js
+++ capitolo1/3/lezione14/js/index.js
@@ -1,20 +1,31 @@
 // Conversione implicita
 
-const nome = "Enzo";
-const cognome = "Goduria";
-const nomeCompleto = nome + " " + cognome;
-console.log(nomeCompleto);
+// const nome = "Enzo";
+// const cognome = "Goduria";
+// const nomeCompleto = nome + " " + cognome;
+// console.log(nomeCompleto);
 
-const numero = 15;
-const numero2 = 7;
-const risultato = numero + numero2;
-console.log(risultato);
+// const numero = 15;
+// const numero2 = 7;
+// const risultato = numero + numero2;
+// console.log(risultato);
 
-// operazioni con stringhe
-const prova = nome - cognome;
-console.log(prova); //Nan
+// // operazioni con stringhe
+// const prova = nome - cognome;
+// console.log(prova); //Nan
 
-const numero3 = "47";
-const numero4 = 8;
-const risultato2 = numero3 - numero4; // usando + concateniamo
-console.log(risultato2);
+// const numero3 = "47";
+// const numero4 = "8";
+// const risultato2 = numero3 - numero4; // usando + concateniamo
+// console.log(risultato2);
+
+// Esempio ricezione dati da un form
+
+const numero = 22;
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let input = document.getElementById("input").value;
+  input = parseInt(input);
+  console.log(input);
+  console.log(numero + input);
+});
