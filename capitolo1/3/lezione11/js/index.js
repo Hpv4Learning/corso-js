3f25ea0c3caaa693893c7e0de3334535dd990240
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-14 11:09:32 +0200

    Esempio di utilizzo template literal string, usando le costanti create in precedenza

--- capitolo1/3/lezione11/js/index.js
+++ capitolo1/3/lezione11/js/index.js
@@ -1,14 +1,13 @@
 // Concatenazione di stringhe
+// Template literal string ` ` + ${ }
+// Utilizzo carattere di escape
 
-// const nome = "Enzo";
-// const cognome = "Goduria";
+const nome = "Enzo";
+const cognome = "Goduria";
 // let nomeCompleto;
 // nomeCompleto = nome + " " + cognome + ".";
 
 // console.log("Ciao, il tuo nome completo è: " + nome + " " + cognome + ".");
 // console.log("Ciao, il tuo nome completo è: " + nomeCompleto);
 
-const sitoWeb = "udemy";
-const url = "https://www." + sitoWeb + ".com";
-
-console.log(url);
+console.log(`Ciao ${nome} ${cognome}, benvenuto! `);
