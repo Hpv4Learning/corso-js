2d61cdb2b2d76f41e315a5ab3f1da7ec6fec4310
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-16 17:53:09 +0200

    Dimostrazione += e -=

--- capitolo1/3/lezione13/js/index.js
+++ capitolo1/3/lezione13/js/index.js
@@ -2,29 +2,38 @@
 // +=, -=, *=, /=, ++, --, %
 // Modulo %, operatore che ritorna il resto di una divisione
 
+// dimostrazione += e -=
 let numero = 20;
 numero += 10;
-// numero += 10;
-// numero -= 10;
-// numero += 10;
-// numero *= 10;
-// numero++;
-// numero++;
-// numero++;
-// numero++;
-// numero--;
-// numero--;
-
+numero -= 3;
 console.log(numero);
 
-const fettePizza = 10;
-const persone = 4;
-const fetteRimanenti = fettePizza % persone;
+// dimostrazione *= e /=
+let numero2 = 10;
+numero2 *= 10;
+numero2 /= 5;
+console.log(numero2);
 
-console.log(fetteRimanenti);
+// dimostrazione ++
+numero2++;
+numero2++;
+numero2++;
+numero2++;
+console.log(numero2);
 
+// dimostrazione regole di somma algebrica
 const espressione1 = 16 + 7 + 12 * 10;
 const espressione2 = (16 + 7 + 12) * 10;
 
 console.log(espressione1);
 console.log(espressione2);
+
+// dimostrazione modulo %
+const num = 5;
+const risultato = num % 2;
+
+if (risultato > 0) {
+  console.log("Il numero è dispari!");
+} else {
+  console.log("Il numero è pari!");
+}
