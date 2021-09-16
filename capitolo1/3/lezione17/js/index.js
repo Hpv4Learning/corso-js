af9944f753f2d1e8d441590ca826743e05835e35
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-16 19:06:44 +0200

    Tipi di dati che può contenere

--- capitolo1/3/lezione17/js/index.js
+++ capitolo1/3/lezione17/js/index.js
@@ -5,3 +5,17 @@ const esempio = new Set(prova);
 
 console.log(prova);
 console.log(esempio); // dimostrazione differenze con array
+
+const prova2 = [
+  "a",
+  "b",
+  3,
+  3,
+  [1, 2, 3],
+  { nome: "Enzo", cognome: "Goduria" },
+  esempio,
+];
+const esempio2 = new Set(prova2);
+
+console.log(prova2);
+console.log(esempio2); // dimostrazione tipi di dati che può contenere
