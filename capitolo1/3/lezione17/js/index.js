6145db0fd0a82d43010538dcb55eb32f487a2c0f
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-16 19:02:26 +0200

    Creazione set

---
+++ capitolo1/3/lezione17/js/index.js
@@ -0,0 +1,7 @@
+// Set - cos'è e cosa può contenere
+
+const prova = [1, 2, 3, 4, 5, 5, 5, 5];
+const esempio = new Set(prova);
+
+console.log(prova);
+console.log(esempio); // dimostrazione differenze con array
