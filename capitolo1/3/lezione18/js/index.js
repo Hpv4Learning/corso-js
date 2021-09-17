c9d51ce9707bea8ecd0ff334776aa4f6787d83b1
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-09-17 15:50:32 +0200

    Creazione oggetto e simostrazione dei tipi di dato che può contenere

---
+++ capitolo1/3/lezione18/js/index.js
@@ -0,0 +1,17 @@
+// Oggetti
+// Collezione di proprietà e metodi
+// Dot notation
+
+const persona = {
+  nome: "Enzo",
+  cognome: "Goduria",
+  eta: 33,
+  sposato: true,
+  figli: ["Luca", "Paolo"],
+  indirizzo: {
+    via: "Via Roma",
+    civico: 1,
+    cap: 10100,
+    citta: "Napoli",
+  }, // proprietà
+};
