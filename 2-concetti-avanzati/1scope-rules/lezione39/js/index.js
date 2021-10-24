0e2a58c19bc914f9e9461345660b7cf96fd9c8de
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-10-24 12:33:44 +0200

    feat: Lezione39 completa

---
+++ 2-concetti-avanzati/1scope-rules/lezione39/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Operatore Ternario è un espressione che si serve di tre parametri per simulare il comportamento di un if statement.
+ * La differenza sostanziale è che un operatore ternario, essendo un espressione, deve sempre ritornare un valore.
+ */
+
+const numero = 5;
+let conditionalNumber;
+
+if (numero > 6) {
+  conditionalNumber = 9;
+} else {
+  conditionalNumber = 4;
+}
