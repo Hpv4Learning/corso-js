6021bd1e0a3488c2cd989cfffb6ed7eb6327bdc5
Omar De Angelis <omar.hpvfilm@gmail.com>
2021-12-18 13:02:01 +0100

    fix: after recording cap.2

--- 2-concetti-avanzati/1scope-rules/lezione39/js/finale.js
+++ 2-concetti-avanzati/1scope-rules/lezione39/js/finale.js
@@ -12,22 +12,21 @@ if (numero > 6) {
   conditionalNumber = 4;
 }
 
-// Operatore Ternario : condizione ? risultato se true : risultato se false
 let ternaryNumber = numero > 6 ? 9 : 4;
+
 console.log(ternaryNumber);
 
-//Differenza
-if (numero > 6) {
+if (numero > 3) {
   console.log("true");
 }
 
-//Non puoi gestire anche il caso di false
-// numero > 6 ? console.log("true");
+numero > 3 ? console.log("true") : console.log("false");
 
 let ternaryNumber2 =
   numero <= 0
     ? false
     : numero > 0 && numero < 6
-    ? "Tra 1 e 5"
+    ? "tra 1 e 5"
     : "maggiore di 6";
+
 console.log(ternaryNumber2);
