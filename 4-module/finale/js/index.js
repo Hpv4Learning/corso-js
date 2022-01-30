f7892e97d04949129f8278261b7ff4a62c1f248e
Omar De Angelis <omar.hpvfilm@gmail.com>
2022-01-30 17:16:09 +0100

    complete starter lection

--- 4-module/finale/js/index.js
+++ 4-module/finale/js/index.js
@@ -2,3 +2,37 @@
 //Named Export
 //Deafult Export
 //React project type
+
+const data = [
+  {
+    name: "Mario Cemento",
+    jobs: "Developer",
+  },
+  {
+    name: "Elena Malizia",
+    jobs: "Head of Designer",
+  },
+  {
+    name: "Matteo Garbin",
+    jobs: "Marketing Manager",
+  },
+];
+
+const element = {
+  btn: document.querySelector(".button-md"),
+  container: document.querySelector("#jobs"),
+};
+
+function showPeople() {
+  let jobsList = data.map(function (element) {
+    return `<p> ${element.name} position:
+        <strong> ${element.jobs} </strong>
+        </p>`;
+  });
+
+  element.container.classList.add("paper");
+
+  element.container.innerHTML = jobsList.join("");
+}
+
+element.btn.addEventListener("click", showPeople);
