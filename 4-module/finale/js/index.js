779ac5c26dd37994c65d7ce7629ebdbf382c81d9
Omar De Angelis <omar.hpvfilm@gmail.com>
2022-01-30 17:21:04 +0100

    named export

--- 4-module/finale/js/index.js
+++ 4-module/finale/js/index.js
@@ -2,26 +2,7 @@
 //Named Export
 //Deafult Export
 //React project type
-
-const data = [
-  {
-    name: "Mario Cemento",
-    jobs: "Developer",
-  },
-  {
-    name: "Elena Malizia",
-    jobs: "Head of Designer",
-  },
-  {
-    name: "Matteo Garbin",
-    jobs: "Marketing Manager",
-  },
-];
-
-const element = {
-  btn: document.querySelector(".button-md"),
-  container: document.querySelector("#jobs"),
-};
+import { data, element } from "./utils/data.js";
 
 function showPeople() {
   let jobsList = data.map(function (element) {
---
+++ 4-module/finale/js/utils/data.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    name: "Mario Cemento",
+    jobs: "Developer",
+  },
+  {
+    name: "Elena Malizia",
+    jobs: "Head of Designer",
+  },
+  {
+    name: "Matteo Garbin",
+    jobs: "Marketing Manager",
+  },
+];
+
+export const element = {
+  btn: document.querySelector(".button-md"),
+  container: document.querySelector("#jobs"),
+};
+
+export { data };
