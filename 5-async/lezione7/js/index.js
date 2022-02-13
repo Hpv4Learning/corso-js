f98b2a4a4cd3965a3c23c399338d8405cf373a93
Omar De Angelis <omar.hpvfilm@gmail.com>
2022-02-13 13:25:57 +0100

    add API excercise

---
+++ 5-async/lezione7/js/index.js
@@ -0,0 +1,6 @@
+// Interagire con un API
+// metodo fetch
+
+const container = document.getElementById("root");
+const url = "https://jsonplaceholder.typicode.com/posts";
+const errorUrl = "https://jsonplaceholder.typicode.com/po";
